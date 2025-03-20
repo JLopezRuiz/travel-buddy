428bdc7b7c44a62efc095a607a5a86b35808fb47
JLopezRuiz <jlopezru@wellesley.edu>
2025-03-20 15:17:12 -0700

    added basic routing for pages

---
+++ src/components/pages/Results/Results.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Results = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Link to="/">Home</Link>
+        </div>
+    );
+};
+
+export default Results;
