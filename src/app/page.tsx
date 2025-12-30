e1365e0cc1c2cdd0116345d85082c05f1da822e3
JLopezRuiz <jlopezru@wellesley.edu>
2025-12-30 10:51:36 -0800

    Trigger initial Vercel deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-3xl font-semibold">
-        ✈️ Travel Buddy
+        ✈️ Travel Buddy - comming soon!
       </h1>
     </main>
   );
